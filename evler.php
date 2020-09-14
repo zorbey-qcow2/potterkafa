abee5dfa9eb9271acb568e05501e97c8472cd25a
zorbey-qcrow2 <zorbey@tuta.io>
2020-09-14 04:05:02 +0300

    Ev API

--- evler.php
+++ evler.php
@@ -5,6 +5,20 @@ function baslikCek()
     return 'Evler';
 }
 
+$evler = file_get_contents('https://www.potterapi.com/v1/houses?key=$2a$10$x4.Fv0K2Pjf.QN44jS.hOeNTuVrkIbOHt8YLKaLkq/cvRwPXiS3/G');
+
+$cozulmusEvler = json_decode($evler,true);
+
+$evIsim = [];
+
+foreach ($cozulmusEvler as $ev) {
+$evIsim[] = $ev['name'];
+}
+
+//die(var_dump($evIsim));
+
+
+
 ?>
 
 <?php
@@ -15,6 +29,11 @@ include 'ustmenu.php';
 <h1><?php echo baslikCek(); ?></h1>
 
 <?php
+
+foreach ($evIsim as $ewisim) {
+    echo $ewisim . "<br>";
+}
+
 include 'footer.php';
 ?>
 
